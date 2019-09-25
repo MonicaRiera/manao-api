65822a6b9b9314de0254fc9c90b04e9ce72963c4
Monica <monicarigi@hotmail.com>
2019-09-25 13:08:59 +0700

    Added database and mongoose

---
+++ database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+let connection = 'mongodb+srv://Monica:Manao123@manao-ngevb.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(connection, {useNewUrlParser:true}, (err) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log('Connected to MongoDB')
+	}
+})
+
+module.exports = mongoose
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const database = require('./database')
 let app = express()
 
 app.get('/', require('./controllers/root'))
