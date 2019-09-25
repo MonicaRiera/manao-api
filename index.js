6b872dfbe7fd732655eeb514db79cafba77e92e7
Ly <lydialawli@gmail.com>
2019-09-25 14:08:48 +0700

    added dotenv for security

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const database = require('./database')
 let app = express()
 
 app.get('/', require('./controllers/root'))
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
 	console.log('Ready on port 4000')
 })
