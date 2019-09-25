688eb755cc8a976351af5485a0cf9df8d85c8c95
Ly <lydialawli@gmail.com>
2019-09-25 15:31:44 +0700

    created Game model

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const database = require('./database')
 let app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
 
 app.get('/', require('./controllers/root'))
+
+
+app.post('/games', require('./controllers/postGame'))
 
 app.listen(process.env.PORT, () => {
 	console.log('Ready on port 4000')
