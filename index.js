c79c4730693bf1ece2a7bd55c1031572464b8a8c
Monica <monicarigi@hotmail.com>
2019-09-25 16:59:24 +0700

    History Model

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ const cors = require('cors')
 
 let app = express()
 app.use(cors({credentials: true}))
-app.use(bodyParser.urlencoded({extend: false}))
-app.use(bodyParser.json())
-
-app.use(cors({credentials: true}))
-app.use(bodyParser.urlencoded({extend: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get('/', require('./controllers/root'))
 app.post('/quizzes', require('./controllers/postQuiz'))
 app.post('/games', require('./controllers/postGame'))
+app.post('/history', require('./controllers/postHistory'))
 
 
 app.listen(process.env.PORT, () => {
