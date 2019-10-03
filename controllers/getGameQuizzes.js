86b9b07458e943d0302b5e5166375ff675101675
Monica <monicarigi@hotmail.com>
2019-10-03 17:24:07 +0700

    fixed quiz population

--- controllers/getGameQuizzes.js
+++ controllers/getGameQuizzes.js
@@ -2,7 +2,7 @@ const Game = require('../models/game')
 
 module.exports = (req, res) => {
 	Game.findOne({_id: req.params.id})
-	.select('quizzes').populate('quizzes')
+	.select('quizzes').populate('quizzes.quiz')
 	.then(game => res.send(game))
 	.catch(error => res.send(error))
 }
