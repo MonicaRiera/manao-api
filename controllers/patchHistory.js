b3e3576d84241d1d3a9563de52b29e28e148968a
lydialawli <lydialawli@gmail.com>
2019-10-07 09:29:43 +0700

    created patchHistory

---
+++ controllers/patchHistory.js
@@ -0,0 +1,23 @@
+const History = require('../models/history')
+const User = require('../models/user')
+
+
+module.exports = (req, res) => {
+
+	History.findOne({_id: req.params.historyId})
+  .then()
+
+	})
+	.catch(error => res.send(error))
+}
+
+//
+// GOAL
+// update user score
+//
+// with history id => find History (req.body.history_id)
+//
+// with userid => find players with user_id  (req.body.user_id)
+//
+// update player.score with given params (req.body.score)
+//   score
