434277dc4bd90a7e5f091068b4f7469dd2a3bac3
Monica <monicarigi@hotmail.com>
2019-10-07 20:09:25 +0700

    Update postHistory.js

--- controllers/postHistory.js
+++ controllers/postHistory.js
@@ -3,7 +3,7 @@ const History = require('../models/history')
 module.exports = (req, res) => {
 	History.create(req.body)
 	.then(data => {
-		res.send(data)
+		res.send(data._id)
 	})
 	.catch(error => {
 		res.send(error)
