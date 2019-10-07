24db4def06fd998322516ab17afa053f746e0f18
lydialawli <lydialawli@gmail.com>
2019-10-07 10:52:02 +0700

    find all history

--- controllers/patchHistory.js
+++ controllers/patchHistory.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 
 module.exports = (req, res) => {
 
-	History.findOne({_id: req.params.historyId}).lean()
+	History.find({}).lean()
   .then(history => res.send(history))
 	.catch(error => res.send(error))
 }
